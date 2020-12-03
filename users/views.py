060c2341eb3def1de034c93c4fac876bba9a0d50
mini <mini13.1994@gmail.com>
2020-12-03 12:53:22 +0530

    myrecipeproject initial commit on 3/12/2020

---
+++ users/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from users.forms import UserRegistrationForm
+
+# Create your views here.
+def register(request):
+    form=UserRegistrationForm()
+    context={}
+    context["form"]=form
+    if request.method=='POST':
+        form=UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request,"users/login.html",context)
+        else:
+            context["form"]=form
+            return render(request,"users/registration.html",context)
+    return render(request,"users/registration.html",context)
+
+
